bb08653205bb8ac760e76827f8e127c2edff6993
Peter Seibel <peter@gigamonkeys.com>
2025-11-30 22:12:54 -0800

    Day 1, part 2

--- src/net/berkeley/peterseibel/AdventOfCode.java
+++ src/net/berkeley/peterseibel/AdventOfCode.java
@@ -6,11 +6,11 @@ public class AdventOfCode {
 
   public static void main(String[] args) throws Exception {
     IO.println("Welcome to Advent of Code!");
-    // Look at the inputs directory and find the most recent inputs.
-    // Default to running solution for the latest input if there is one.
 
     Solution<Path, Integer> day1 = new Day01_SecretEntrance();
-    IO.println(day1.part1(Path.of("inputs/day-01/puzzle-1.txt")));
+    //IO.println(day1.part1(Path.of("inputs/day-01/puzzle-1.txt")));
+    IO.println(day1.part2(Path.of("inputs/day-01/test-1.txt")));
+    IO.println(day1.part2(Path.of("inputs/day-01/puzzle-1.txt")));
 
   }
 }
--- src/net/berkeley/peterseibel/Day01_SecretEntrance.java
+++ src/net/berkeley/peterseibel/Day01_SecretEntrance.java
@@ -4,11 +4,9 @@ import module java.base;
 
 import static java.nio.file.Files.lines;
 import static java.lang.Integer.parseInt;
-import static java.lang.Math.floorMod;
+import static java.lang.Math.*;
 
 public class Day01_SecretEntrance implements Solution<Path, Integer> {
-
-
 
   public Integer part1(Path input) throws IOException {
     int p = 50;
@@ -20,6 +18,23 @@ public class Day01_SecretEntrance implements Solution<Path, Integer> {
     return count;
   }
 
+  public Integer part2(Path input) throws IOException {
+    int p = 50;
+    int count = 0;
+    for (int n : lines(input).mapToInt(this::parse).toArray()) {
+      //IO.println("Rotating " + n);
+      if (n > 0 && (p + n) >= 100) {
+        count += 1 + (n - (100 - p)) / 100;
+      } else if (n < 0 && (p + n) <= 0) {
+        int first = p > 0 ? 1 : 0;
+        count += first + (abs(n) - p) / 100;
+      }
+      // if n == 0 we didn't turn at all so it shouldn't change, I don't think
+      p = floorMod(p + n, 100);
+      //IO.println("p: %d; count: %d".formatted(p, count));
+    }
+    return count;
+  }
 
   private int parse(String line) {
     int n = parseInt(line.substring(1));
