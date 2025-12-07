58b0d94711f56b775cfd3c7980dd90e2077909c4
Peter Seibel <peter@gigamonkeys.com>
2025-12-06 21:54:51 -0800

    Some cleanup.

--- src/net/berkeley/peterseibel/Day07_Laboratories.java
+++ src/net/berkeley/peterseibel/Day07_Laboratories.java
@@ -1,6 +1,7 @@
 package net.berkeley.peterseibel;
 
 import static java.lang.Math.*;
+import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.*;
 
 import module java.base;
@@ -14,15 +15,15 @@ public class Day07_Laboratories extends Solution<List<String>, Long> {
   public Long part1(List<String> lines) {
     long count = 0L;
 
-    boolean[] beams = start(lines.getFirst());
+    boolean[] beams = booleans(lines.getFirst(), 'S');
     for (String line : lines.subList(1, lines.size())) {
-      var splitters = splitters(line);
+      var splitters = booleans(line, '^');
       for (int i = 0; i < splitters.length; i++) {
         if (beams[i] && splitters[i]) {
           count++;
-          beams[i] = false;
           if (i > 0) beams[i - 1] = true;
           if (i < beams.length - 1) beams[i + 1] = true;
+          beams[i] = false;
         }
       }
     }
@@ -32,7 +33,7 @@ public class Day07_Laboratories extends Solution<List<String>, Long> {
   public Long part2(List<String> lines) {
     long[] paths = startPaths(lines.getFirst());
     for (String line : lines.subList(1, lines.size())) {
-      var splitters = splitters(line);
+      var splitters = booleans(line, '^');
       for (int i = 0; i < splitters.length; i++) {
         if (paths[i] > 0 && splitters[i]) {
           if (i > 0) paths[i - 1] += paths[i];
@@ -41,27 +42,15 @@ public class Day07_Laboratories extends Solution<List<String>, Long> {
         }
       }
     }
-    long count = 0L;
-    for (int i = 0; i < paths.length; i++) {
-      count += paths[i];
-    }
-    return count;
+
+    return stream(paths).sum();
   }
 
-  private boolean[] splitters(String line) {
+  private boolean[] booleans(String line, char c) {
     char[] chars = line.toCharArray();
     boolean[] r = new boolean[chars.length];
     for (int i = 0; i < chars.length; i++) {
-      r[i] = (chars[i] == '^');
-    }
-    return r;
-  }
-
-  private boolean[] start(String line) {
-    char[] chars = line.toCharArray();
-    boolean[] r = new boolean[chars.length];
-    for (int i = 0; i < chars.length; i++) {
-      r[i] = (chars[i] == 'S');
+      r[i] = chars[i] == c;
     }
     return r;
   }
