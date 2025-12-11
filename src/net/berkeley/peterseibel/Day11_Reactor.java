737a5bd8e6874deb2723125f4d98c3bfd16212fe
Peter Seibel <peter@gigamonkeys.com>
2025-12-10 22:49:06 -0800

    Put main code in Paths class.

--- src/net/berkeley/peterseibel/Day11_Reactor.java
+++ src/net/berkeley/peterseibel/Day11_Reactor.java
@@ -1,7 +1,6 @@
 package net.berkeley.peterseibel;
 
 import static java.lang.Math.*;
-import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.*;
 import static java.util.stream.Gatherers.*;
 
@@ -9,88 +8,96 @@ import module java.base;
 
 public class Day11_Reactor extends Solution<List<String>, Long> {
 
+  static class Paths {
+    private final Map<String, List<String>> graph;
+    private final List<String> sorted;
+
+    Paths(List<String> lines) {
+      graph = loadGraph(lines);
+      sorted = topoSort();
+    }
+
+    public long ways(String start, String end) {
+      Map<String, Long> ways = new HashMap<>();
+      ways.put(start, 1L);
+
+      for (var n : sorted) {
+        if (graph.containsKey(n)) {
+          for (var o : graph.get(n)) {
+            ways.merge(o, ways.getOrDefault(n, 0L), (a, b) -> a + b);
+          }
+        }
+      }
+      return ways.get(end);
+    }
+
+    private List<String> topoSort() {
+      var indegree = indegree(graph);
+
+      // Put starting points into queue to be sorted first.
+      Queue<String> queue = new LinkedList<>();
+      for (var n : graph.keySet()) {
+        if (indegree.getOrDefault(n, 0) == 0) {
+          queue.add(n);
+        }
+      }
+
+      // Pull items from queue to add to sorted list and decrement the indegree of
+      // their neighbors.
+      List<String> sorted = new ArrayList<>();
+      while (!queue.isEmpty()) {
+        String n = queue.remove();
+        sorted.add(n);
+        if (graph.containsKey(n)) {
+          for (var o : graph.get(n)) {
+            if (indegree.computeIfPresent(o, (_, v) -> v - 1) == 0) {
+              queue.add(o);
+            }
+          }
+        }
+      }
+      return sorted;
+    }
+
+    private Map<String, Integer> indegree(Map<String, List<String>> m) {
+      Map<String, Integer> indegree = new HashMap<>();
+      for (var v : graph.values()) {
+        for (var n : v) {
+          indegree.merge(n, 1, (a, b) -> a + b);
+        }
+      }
+      return indegree;
+    }
+  }
+
   public Day11_Reactor() {
     super(11, Data::asLines, Data::asLong);
   }
 
   public Long part1(List<String> lines) {
-    Map<String, List<String>> m = loadMap(lines);
-    return ways("you", "out", m, topoSort(m));
+    return new Paths(lines).ways("you", "out");
   }
 
   public Long part2(List<String> lines) {
-    Map<String, List<String>> m = loadMap(lines);
-    List<String> sorted = topoSort(m);
+    Paths p = new Paths(lines);
+    long fft2dac = p.ways("fft", "dac");
 
-    long fft2dac = ways("fft", "dac", m, sorted);
-
+    // Counting on this being a DAG
     if (fft2dac > 0) {
-      long svr2fft = ways("svr", "fft", m, sorted);
-      long dac2out = ways("dac", "out", m, sorted);
-      return svr2fft * fft2dac * dac2out;
+      return p.ways("svr", "fft") * fft2dac * p.ways("dac", "out");
     } else {
-      long svr2dac = ways("svr", "dac", m, sorted);
-      long dac2fft = ways("dac", "fft", m, sorted);
-      long fft2out = ways("fft", "out", m, sorted);
-      return svr2dac * dac2fft * fft2out;
+      return p.ways("svr", "dac") * p.ways("dac", "fft") * p.ways("fft", "out");
     }
   }
 
-  private Map<String, Integer> indegree(Map<String, List<String>> m) {
-    Map<String, Integer> indegree = new HashMap<>();
-    for (var v : m.values()) {
-      for (var n : v) {
-        indegree.merge(n, 1, (a, b) -> a + b);
-      }
-    }
-    return indegree;
-  }
-
-  private List<String> topoSort(Map<String, List<String>> m) {
-    var indegree = indegree(m);
-    // Put starting points into queue to be sorted first.
-    Queue<String> queue = new LinkedList<>();
-    for (var n : m.keySet()) {
-      if (indegree.getOrDefault(n, 0) == 0) {
-        queue.add(n);
-      }
-    }
-
-    // Pull items from queue to add to sorted list and decrement the indegree of
-    // their neighbors.
-    List<String> sorted = new ArrayList<>();
-    while (!queue.isEmpty()) {
-      String n = queue.remove();
-      sorted.add(n);
-      for (var o : m.getOrDefault(n, List.of())) {
-        if (indegree.computeIfPresent(o, (_, v) -> v - 1) == 0) {
-          queue.add(o);
-        }
-      }
-    }
-    return sorted;
-  }
-
-  private long ways(String start, String end, Map<String, List<String>> m, List<String> sorted) {
-    Map<String, Long> ways = new HashMap<>();
-    ways.put(start, 1L);
-
-    for (var n : sorted) {
-      for (var o : m.getOrDefault(n, List.of())) {
-        ways.merge(o, ways.getOrDefault(n, 0L), (a, b) -> a + b);
-      }
-    }
-    return ways.get(end);
-  }
-
-  private Map<String, List<String>> loadMap(List<String> lines) {
-    Map<String, List<String>> m = new HashMap<>();
+  private static Map<String, List<String>> loadGraph(List<String> lines) {
+    Map<String, List<String>> graph = new HashMap<>();
     for (var line : lines) {
       int colon = line.indexOf(":");
-      String from = line.substring(0, colon).intern();
-      List<String> to = stream(line.substring(colon + 2).split(" ")).map(String::intern).toList();
-      m.put(from, to);
+      String from = line.substring(0, colon);
+      List<String> to = Arrays.asList(line.substring(colon + 2).split(" "));
+      graph.put(from, to);
     }
-    return m;
+    return graph;
   }
 }
